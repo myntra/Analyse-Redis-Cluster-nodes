8bd6fbfdbfce287af046439438d4f5c9b3e7a1ee
Siddharth Agarwal <sidd081@users.noreply.github.com>
2017-02-22 11:41:55 +0530

    Update fetchRedisClusterInfo.sh
    To check if master and slave are equal in number on nodes.

--- fetchRedisClusterInfo.sh
+++ fetchRedisClusterInfo.sh
@@ -25,7 +25,7 @@ done
 
 echo "HOST: " $host  " PORT: " $port
 
-redis-cli -c -h $host -p $port cluster nodes | awk  -F: '{print $1 " " $2}' | awk  ' {if ($4 ~ /master/){freq[$2, "master"]++; machine[$2]++} else if($4 ~ /slave/){freq [$2,"slave"]++;machine[$2]++} else{freq [$2, $4]++;machine[$2]++} } END{printf("%-15s %8s %8s \n" ,"machine", "master", "slave"); for (var in machine) {printf("%-15s %8.2f %8.2f \n", var, freq[var, "master"], freq[var, "slave"])}}'
+redis-cli -c -h $host -p $port cluster nodes | awk  -F: '{print $1 " " $2}' | awk  ' {if ($4 ~ /master/){freq[$2, "master"]++; machine[$2]++} else if($4 ~ /slave/){freq [$2,"slave"]++;machine[$2]++} else{freq [$2, $4]++;machine[$2]++} } END{printf("%-15s %8s %8s \n" ,"machine", "master", "slave"); for (var in machine) { if (freq[var, "master"] !=  freq[var, "slave"]) {printf("\033[1;31m%-15s %8.2f %8.2f \033[0m\n", var, freq[var, "master"], freq[var, "slave"])} else { printf("\033[1;32m%-15s %8.2f %8.2f \033[0m\n", var, freq[var, "master"], freq[var, "slave"])}}}'
 echo -e "\n\nMASTER SLAVE INFO"
 #redis-cli  -c -p 7001 cluster nodes | awk '{printf "%-25s  %-15s %s\n", $2, $3, $8}' | sort
 #echo -e " \n\n\n"
